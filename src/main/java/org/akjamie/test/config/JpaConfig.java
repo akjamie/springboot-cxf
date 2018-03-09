75fc83ed575cf47c59f7d844f33870cc4fc0055a
Jamie Zhang <akjamie.zhang@gmail.com>
2018-03-09 09:52:34 +0800

    remove the jdbc config and replace it with pcf service - mysql

--- src/main/java/org/akjamie/test/config/JpaConfig.java
+++ src/main/java/org/akjamie/test/config/JpaConfig.java
@@ -7,12 +7,14 @@ import javax.sql.DataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ConfigurationProperties(prefix = "spring.datasource")
+@Profile("dev")
 public class JpaConfig extends HikariConfig {
 
     @Bean
